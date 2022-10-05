a1bee9913d1facddd998ba7d92aee345ff64da3f
akkarchaimaa <115052089+akkarchaimaa@users.noreply.github.com>
2022-10-05 10:08:15 +0200

    Add files via upload

---
+++ pycharm/bloc.py
@@ -0,0 +1,6 @@
+i=1
+def test():
+    i=i+1
+    print()
+    test()
+test()
---
+++ pycharm/test2.py
@@ -0,0 +1,12 @@
+def my_function():
+d=input("donner densité")
+v=input("donner viscosité cinématique")
+try:
+    d=float(d)
+    v=float(v)
+    u=v*0.0001*1000*d
+    print("la viscosité dynamique est:",u,"PI")
+except:
+    print("la valeur de densité et/ou viscosité que vous avez donné pas correctr")
+    my_function()
+my_function()
